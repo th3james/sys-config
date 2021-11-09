cde10df8245aa705e1813f417a7b1c539d897fc2
James Cox-Morton <th3james@fastmail.fm>
2021-11-09 14:17:01 +0000

    Make veex handle pipenv venvs

--- config/fish/functions/veex.fish
+++ config/fish/functions/veex.fish
@@ -1,4 +1,11 @@
 function veex -d "exec in venv"
-  source .venv/bin/activate.fish
+  if is_installed pipenv
+    set -l venv_dir (pipenv --venv 2> /dev/null)
+  end
+  if not string length -q venv_dir
+    set -l venv_dir ".venv"
+  end
+
+  source $venv_dir/bin/activate.fish
   $argv
 end
