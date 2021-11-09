d0066e7a6a40c033df0de9a5e7351624030c6d52
James Cox-Morton <th3james@fastmail.fm>
2021-11-09 14:27:28 +0000

    Use fish variable scope for venv_dir

--- config/fish/functions/veex.fish
+++ config/fish/functions/veex.fish
@@ -1,9 +1,9 @@
 function veex -d "exec in venv"
   if is_installed pipenv
-    set -l venv_dir (pipenv --venv 2> /dev/null)
+    set venv_dir (pipenv --venv 2> /dev/null)
   end
-  if not string length -q venv_dir
-    set -l venv_dir ".venv"
+  if not string length -q $venv_dir
+    set venv_dir ".venv"
   end
 
   source $venv_dir/bin/activate.fish
