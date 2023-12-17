21890e1c5cd96ca741f27ef16cde187e1302a144
James Cox-Morton <james.cox-morton@arm.com>
2023-12-17 15:58:17 +0000

    Configure clojure linting

--- config/nvim/lua/linters.lua
+++ config/nvim/lua/linters.lua
@@ -1,24 +1,25 @@
-local lint = require('lint')
+local lint = require("lint")
 
 lint.linters_by_ft = {
-  lua = {'luacheck',}
+	lua = { "luacheck" },
+	clojure = { "clj-kondo" },
 }
 
 local luacheck = lint.linters.luacheck
 luacheck.args = {
-  "--formatter",
-  "plain",
-  "--codes",
-  "--ranges",
-  "--filename",
-  "$FILENAME",
-  "-",
-  "--globals",
-  "vim",
+	"--formatter",
+	"plain",
+	"--codes",
+	"--ranges",
+	"--filename",
+	"$FILENAME",
+	"-",
+	"--globals",
+	"vim",
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
+	callback = function()
+		require("lint").try_lint()
+	end,
 })
