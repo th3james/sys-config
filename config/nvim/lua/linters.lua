dfc7a12ec4a52bb9776cd20d22f1047af1431124
James Cox-Morton <james.cox-morton@arm.com>
2024-02-09 17:01:36 +0000

    Fix cppcheck

--- config/nvim/lua/linters.lua
+++ config/nvim/lua/linters.lua
@@ -24,6 +24,29 @@ luacheck.args = {
 	"vim",
 }
 
+local cppcheck = lint.linters.cppcheck
+-- https://github.com/mfussenegger/nvim-lint/issues/492
+cppcheck.args = {
+	"--enable=warning,style,performance,information",
+	function()
+		if vim.bo.filetype == "cpp" then
+			return "--language=c++"
+		else
+			return "--language=c"
+		end
+	end,
+	"--inline-suppr",
+	"--quiet",
+	function()
+		if vim.fn.isdirectory("build") == 1 then
+			return "--cppcheck-build-dir=build"
+		else
+			return ""
+		end
+	end,
+	"--template={file}:{line}:{column}: [{id}] {severity}: {message}",
+}
+
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 	callback = function()
 		require("lint").try_lint()
