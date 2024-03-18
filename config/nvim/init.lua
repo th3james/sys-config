7775543ed79ed2195eb8364d73deb8632d3231c2
James Cox-Morton <james.cox-morton@arm.com>
2024-03-18 22:09:27 +0000

    Disable hotpot

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,10 +1,3 @@
-local hotpot_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/hotpot.nvim"
-if vim.fn.empty(vim.fn.glob(hotpot_path)) > 0 then
-	print("Could not find hotpot.nvim, cloning new copy to", hotpot_path)
-	vim.fn.system({ "git", "clone", "https://github.com/rktjmp/hotpot.nvim", hotpot_path })
-	vim.cmd("helptags " .. hotpot_path .. "/doc")
-end
-
 require("options")
 require("plugins")
 require("formatters")
