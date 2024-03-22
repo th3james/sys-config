72371c73eb8e9a7d46f6472c4623edcf78e3a472
James Cox-Morton <james.cox-morton@arm.com>
2024-03-22 09:40:48 +0000

    Add sleuth and whichkey

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -12,6 +12,25 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+
+	{ -- Useful plugin to show you pending keybinds.
+		"folke/which-key.nvim",
+		event = "VimEnter", -- Sets the loading event to 'VimEnter'
+		config = function() -- This is the function that runs, AFTER loading
+			require("which-key").setup()
+
+			-- Document existing key chains
+			require("which-key").register({
+				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+			})
+		end,
+	},
+
 	"neovim/nvim-lspconfig",
 	"mfussenegger/nvim-lint",
 	"mhartington/formatter.nvim",
