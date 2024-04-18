b682118fa6e6328566793b175f0dfdd9de58368e
James Cox-Morton <james.cox-morton@arm.com>
2024-04-18 14:51:02 +0100

    Add JSON and YAML LSP

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -9,7 +9,47 @@ lspconfig.tsserver.setup({})
 -- lspconfig.terraformls.setup({})
 lspconfig.clangd.setup({})
 lspconfig.gopls.setup({})
+-- brew install vscode-langservers-extracted
+lspconfig.jsonls.setup({
+	json = {
+		schemas = {
+			{
+				fileMatch = { "package.json" },
+				url = "https://json.schemastore.org/package.json",
+			},
+			{
+				fileMatch = { "tsconfig*.json" },
+				url = "https://json.schemastore.org/tsconfig.json",
+			},
+			{
+				fileMatch = { ".eslintrc", ".eslintrc.json" },
+				url = "https://json.schemastore.org/eslintrc.json",
+			},
+		},
+	},
+})
 lspconfig.pyright.setup({})
+-- brew install yaml-language-server
+lspconfig.yamlls.setup({
+	settings = {
+		yaml = {
+			validate = true,
+			-- disable the schema store
+			schemaStore = {
+				enable = false,
+				url = "",
+			},
+			-- manually select schemas
+			-- source: https://www.schemastore.org/api/json/catalog.json
+			schemas = {
+				["https://raw.githubusercontent.com/docker/compose/master/compose/config/compose_spec.json"] = "docker-compose*.{yml,yaml}",
+				["https://json.schemastore.org/dependabot-2.0.json"] = "dependabot*.{yml,yaml}",
+				["https://json.schemastore.org/github-action.json"] = "actions*.{yml,yaml}",
+				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+			},
+		},
+	},
+})
 lspconfig.zls.setup({})
 
 -- local rt = require("rust-tools")
