1779fa04e54ab44faa107a2112438b1e58156b53
James Cox-Morton <james.cox-morton@arm.com>
2024-06-25 17:05:06 +0100

    These are default in nvim 0.10

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -8,8 +8,6 @@ vim.keymap.set("i", "jk", "<Esc>", { noremap = true })
 vim.keymap.set("i", "uu", "<Esc>", { noremap = true })
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
