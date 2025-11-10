106b4b69e61bb5ec27b5a540ced476460b8adf42
James Cox-Morton (th3james) <th3james@hey.com>
2025-11-10 13:21:19 +0000

    Add signing config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,6 +14,8 @@ set -gx fish_user_paths "$HOME/src/sys-config/zig-scripts/zig-out/bin" $fish_use
 set -gx fish_user_paths "$HOME/.config/bun-scripts/" $fish_user_paths
 set -gx fish_user_paths "$HOME/src/FVPs-on-Mac/bin" $fish_user_paths
 
+set -gx GPG_TTY (tty)
+
 # Use ripgrep with fzf
 set -gx FZF_DEFAULT_COMMAND "rg --files --hidden -g \"!.git/\""
 
