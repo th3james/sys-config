020250580a50bbbdf85163ebab7ee90366b9d27a
James Cox-Morton (th3james) <th3james@hey.com>
2025-11-14 09:07:29 +0000

    Use modern git commands

--- config/fish/functions/aliases.fish
+++ config/fish/functions/aliases.fish
@@ -4,15 +4,14 @@ abbr -a -g vi nvim
 # Git #
 abbr -a -g ga git add
 abbr -a -g gst git status
-abbr -a -g gcm 'git checkout (gmom)'
-abbr -a -g gco git checkout
+abbr -a -g gsm 'git switch (gmom)'
+abbr -a -g gsw git switch
 abbr -a -g gd git diff
 abbr -a -g gc git commit -v
 abbr -a -g gfm 'git fetch; git merge --ff-only'
 abbr -a -g gfr 'git fetch; git rebase'
 abbr -a -g gpo 'git push origin'
 abbr -a -g gph 'git push origin HEAD'
-abbr -a -g glp 'git log --graph --pretty=oneline --abbrev-commit'
 
 # Kubectl #
 abbr -a -g kcnls 'kubectl get namespaces'
